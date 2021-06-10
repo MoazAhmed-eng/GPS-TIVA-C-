5b83f2fb5f6a2e5c22eb91d0457047ca31d8484d
Youssefwaleed1 <85322478+Youssefwaleed1@users.noreply.github.com>
2021-06-10 11:04:49 +0200

    Update uart_init.c

--- uart_init.c
+++ uart_init.c
@@ -4,32 +4,33 @@
 
 void uart_init(void)
 {
-//This better will need to get edited as pins A0,A1 are not visible on the board itself	
-//UART initialization (A0,A1)
+// initialzing UART1 which corresponds to PC4 and PC5	
+// This initialization is based on the following : 1. tiva frequency = 16Mhz   2. BR of neo 6m = 9600 bps (default BR )
 
-//Initalizing Clock for UART0
-SYSCTL_RCGCGPIO_R|=0x01;
-SYSCTL_RCGCUART_R|=0x01;
- // while((SYSCTL_PRUART_R&(0x01))==0);
+// Seting the clock for both UART1 and Port c
 	
-	// Enabling Digital For Pins A0,A1
-GPIO_PORTA_DEN_R|=0x03;
-//Disabling Analog
-GPIO_PORTA_AMSEL_R &=~0x03;
-//Enabling AFSEL
-GPIO_PORTA_AFSEL_R&=0x03;
-//Choosing the wanted function
-GPIO_PORTA_PCTL_R|=0x00000011;
-//Selecting the direction
-GPIO_PORTA_DIR_R|=0x02;
-//Disabling
-UART0_CTL_R&=~0x0001;
-//Adding the values for the integer and fractional baud rate
-UART0_IBRD_R|=104;
-UART0_FBRD_R|=11;
-//determining the length of the data as 8 bits and one stop bit and FIFO mode ON
-UART0_LCRH_R|=0x70;
-//Now Enabling
-UART0_CTL_R=0x0301;
+SYSCTL_RCGCGPIO_R |= 0x02 ; 
+SYSCTL_RCGCUART_R |= 0x04 ;
+	
+// Disabling UART1 
+	
+UART0_CTL_R &= ~ (0x01) ;
 
+// UART1 configuration
+
+UART0_LCRH_R = 0x70 ; // fifo enabled (bit4=1) and data length is 8 bits ( bit5 = bit6 = 1 )
+UART0_IBRD_R = 140 ;	// divisor = (16M/(16*9600)) = 104.167  1) IBRD = 104   2) FBRD = (0.167*64) + 0.5 = 11 
+UART0_FBRD_R = 11 ;	
+	
+// Enabling UART1 , TX and RX 
+
+UART0_CTL_R |= 0x301 ; // UART1 enabled (bit0=1) and RX and TX are enabled (bit4=bit5=1)
+
+//  Port C configuration
+	
+GPIO_PORTC_AFSEL_R |= 0x30 ;
+GPIO_PORTC_PCTL_R = (GPIO_PORTC_PCTL_R & 0xFF00FFFF ) + 0x00220000 ; // Determining pic C4 and C5 as UART1
+GPIO_PORTC_DEN_R |= 0x30 ;
+GPIO_PORTC_AMSEL_R &= ~ (0x30) ;
+	
 }
