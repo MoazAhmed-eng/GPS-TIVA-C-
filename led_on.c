5436810e6112c7c81317d7c6301e728de11884b0
Amr <63327970+HelalyJunior@users.noreply.github.com>
2021-06-16 00:59:47 +0200

    Update led_on.c

--- led_on.c
+++ led_on.c
@@ -1,7 +1,7 @@
 #include "tm4c123gh6pm.h"
 #include "func_prototype.h"
 
-void turnOnRedLed( char distance)
+void turnOnRedLed( double distance)
 {
 if (distance>=100)
 					GPIO_PORTF_DATA_R|=0x02;
