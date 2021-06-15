085e34c55d87f3f5fa9c90f27eef62b617dbd192
MoazAhmed-eng <85304515+MoazAhmed-eng@users.noreply.github.com>
2021-06-15 19:04:53 +0200

    Update GPS_Receive.c

--- GPS_Receive.c
+++ GPS_Receive.c
@@ -1,34 +1,49 @@
 #include "func_prototype.h"
 #include "stdint.h"
-#include "GPS_Receive.h"
+#include "func_prototype.h"
 
-  char* get_NEMA_str()
+char* get_NEMA_str()
 {
-	static  char tmp_str[100];
+	int i =0;
+	int p =1;
+	static  char tmp_str[50];
 	 char received;
-	int loop=1;
+while (p){
+	 received = UART1_Read();
+	if(received=='$') {
 	
-	
-	
-	while(loop)
-		
-	{
-	int j;
-	received=UART1_Read(); //First Element
-	for(j=0;((j<100)&&(received!=0x0A));j++)
-	{
-		tmp_str[j] = received;
-		received=UART1_Read();
+		received = UART1_Read();
+
+		if(received=='G') {
+
+						received = UART1_Read();
+
+
+				if(received=='P') {
+						received = UART1_Read();
+
+						if(received=='R') {
+											received = UART1_Read();
+																if(received=='M') {
+																					received = UART1_Read();
+
+																							if(received=='C') {
+																										received = UART1_Read();
+
+																												if(received==',') {
+
+																													for(i = 0 ; i< 50 ; i++)
+																													{
+																														tmp_str[i]=UART1_Read();
+
+																													}
+																													p =0;
+																												}}}}}}
+
+
 	}
-	
-	tmp_str[j]='\0';
-	
-	if(tmp_str[0]=='$' && tmp_str[3]=='R') //if true -> recieved is GPRMC
-	{
-			loop=0;
-	}
-	
 }
+
 	
 return tmp_str;
 	
